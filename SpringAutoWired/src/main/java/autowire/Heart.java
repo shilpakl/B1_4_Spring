0a16a2dacfd40ba560f5fece28c1ccb2c7ac4553
Shilpa KL <Shilpa KL@LAPTOP-KSCNGTRR>
2022-06-12 14:58:53 +0530

    First Commit

---
+++ SpringAutoWired/src/main/java/autowire/Heart.java
@@ -0,0 +1,39 @@
+package autowire;
+
+public class Heart
+{
+	private String nameOfAnimal;
+	private String noOfHeart;
+	
+	
+	public String getNameOfAnimal()
+	{
+		return nameOfAnimal;
+	}
+
+
+	public void setNameOfAnimal(String nameOfAnimal) 
+	{
+		this.nameOfAnimal = nameOfAnimal;
+	}
+
+
+	public String getNoOfHeart() 
+	{
+		return noOfHeart;
+	}
+
+
+	public void setNoOfHeart(String noOfHeart)
+	{
+		this.noOfHeart = noOfHeart;
+	}
+
+
+	public void pump()
+	{
+		System.out.println("The program is executing");
+		System.out.println("I am alive");
+	}
+
+}
