aca9ec42dc5cc2107bc0d08cfee26fd0b39243d3
Shilpa KL <Shilpa KL@LAPTOP-KSCNGTRR>
2022-06-12 12:10:55 +0530

    First Commit

---
+++ SpringIOC/src/main/java/springioc/Jio.java
@@ -0,0 +1,18 @@
+package springioc;
+
+public class Jio implements Sim
+{
+
+	public void calling() 
+	{
+		System.out.println("Calling using Jio sim");
+		
+	}
+
+	public void data() 
+	{
+		System.out.println("data using Jio sim");
+		
+	}
+
+}
---
+++ SpringIOC/src/main/java/springioc/Sim.java
@@ -0,0 +1,9 @@
+package springioc;
+
+public interface Sim 
+{
+
+	public abstract void calling();
+	public abstract void data();
+
+}
